cef71a915091cf73cba0e80ccf3382c4e918c0ee
mrxbox1 <gabriel.holub@outlook.com>
2026-05-01 14:12:33 +0200

    working on the markov chain

--- scripts/markov_text_label_test.gd
+++ scripts/markov_text_label_test.gd
@@ -8,5 +8,12 @@ var text_split = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	text_split = text_string.replace(" ", "|").replace("\n", "|")
+	text_split = text_split.split("|")
+	
+	for word in len(text_split)-1:
+		#print(text_split[word])
+		text_markovified[text_split[word]] = []
+		
+	print(text_markovified)
 	
 	text = str(text_split)
