fcfff5f62cec0772330cc1755723af2436c9859e
mrxbox1 <gabriel.holub@outlook.com>
2026-05-01 17:16:16 +0200

    random images included!

--- scripts/randomize_theme.gd
+++ scripts/randomize_theme.gd
@@ -1,6 +1,8 @@
 extends Control
 
+var textures = ["cubed.png", "fish.png", "flower.png"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.add_theme_stylebox_override("panel", StyleBoxTexture.new())
+	$TextureRect.texture = load("res://textures/" + textures.pick_random())
+	$TextureRect.modulate = Color(randf(), randf(), randf())
