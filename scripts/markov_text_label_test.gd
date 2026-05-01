1932c9443b3e310535f9882f3c1f5aa503481795
mrxbox1 <gabriel.holub@outlook.com>
2026-05-01 14:29:35 +0200

    i don't know what i'm doinggggggggg

--- scripts/markov_text_label_test.gd
+++ scripts/markov_text_label_test.gd
@@ -3,17 +3,30 @@ extends RichTextLabel
 var text_string = FileAccess.get_file_as_string("texts/sample.txt")
 var text_markovified = {}
 
-var text_split = []
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	text_split = text_string.replace(" ", "|").replace("\n", "|")
-	text_split = text_split.split("|")
+
+func markovify_text(input_text):
+	var text_split = input_text.replace(" ", "|").replace("\n", "|")
+	text_split = input_text.split("|")
 	
 	for word in len(text_split)-1:
 		#print(text_split[word])
 		text_markovified[text_split[word]] = []
 		
-	print(text_markovified)
+		for other_word in len(text_split)-1:
+			if text_split[other_word] == text_split[word]:
+				text_markovified[text_split[word]].append(text_split[other_word+1])
+
+
+
+func create_text():
+	var random_word = text_markovified.keys().pick_random()
+	var resulting_text = ""
 	
-	text = str(text_split)
+	
+
+
+
+func _ready() -> void:
+	print(text_markovified)
+	markovify_text(text_string)
