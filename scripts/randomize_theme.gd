5a142593d7f3ee23b2ec8f4725839958a731ea6b
mrxbox1 <gabriel.holub@outlook.com>
2026-05-13 19:00:18 +0200

    i'm lazy, this is prob gonna be a last commit

--- scripts/randomize_theme.gd
+++ scripts/randomize_theme.gd
@@ -1,9 +1,17 @@
 extends Control
 
 var textures = ["cubed.png", "fish.png", "flower.png", "gener_ation.png",
-				"insanity.png", "it_aint_aligned.png", "not_fish.png", "square_inception.png"]
+				"insanity.png", "it_aint_aligned.png", "not_fish.png", "square_inception.png",
+				"meaningless.png", "squared.png", "landscaped.png", "alphabetfrantic.png",
+				"tunnell.png"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$TextureRect.texture = load("res://textures/" + textures.pick_random())
 	$TextureRect.modulate = Color(randf(), randf(), randf())
+	
+	$Parallax2D/Sprite2D.visible = bool(randi_range(0,1))
+	$Parallax2D/Sprite2D.modulate = Color(randf(), randf(), randf())
+	
+	$Parallax2D.autoscroll.x = randf_range(-20.0, 20.0)
+	$Parallax2D.autoscroll.y = randf_range(-20.0, 20.0)
